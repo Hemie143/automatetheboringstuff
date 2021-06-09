0c553fd69fdce8f2fb99d70c5b2d4cb02cc6ecc6
hemie143 <hemie143@gmail.com>
2021-06-09 09:36:48 +0200

    start of chap 9

--- ch08_input_validation/ex02.py
+++ ch08_input_validation/ex02.py
@@ -38,3 +38,11 @@ response = pyip.inputNum(blockRegexes=[r'[02468]$'])
 response = pyip.inputStr(allowRegexes=[r'caterpillar', 'category'], blockRegexes=[r'cat'])
 
 # custom validation
+def addsUpToTen(numbers):
+    numbersList = list(numbers)
+    for i, digit in enumerate(numbersList):
+        numbersList[i] = int(digit)
+    if sum(numbersList) != 10:
+        raise Exception(f'The digits must add up to 10, not {sum(numbersList)}.')
+
+response = pyip.inputCustom(addsUpToTen)
