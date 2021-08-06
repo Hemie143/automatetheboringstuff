4c4ef2a5f5cd9b74d3b65c515cefc23a5190f93a
hemie143 <hemie143@gmail.com>
2021-08-06 20:44:47 +0200

    chap 9

--- ch09_reading_and_writing_files/ex01.py
+++ ch09_reading_and_writing_files/ex01.py
@@ -7,19 +7,21 @@ print(str(Path('spam', 'bacon', 'eggs')))
 
 myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
 for filename in myFiles:
-    print(Path(r'C:\Users\Al', filename))
+    print(Path(r'/tmp', filename))
 
+cwd = Path.cwd()
 print(Path.cwd())
-# os.chdir('C:\Windows\System32')
+os.chdir('/tmp')
 print(Path.cwd())
+os.chdir(cwd)
 
 # The home directory
 print(Path.home())
 
 # Create new folders
-os.makedirs(r'C:\delicious\walnut\waffles', exist_ok=True)
+os.makedirs(r'/tmp/delicious/walnut/waffles', exist_ok=True)
 
-Path(r'C:\delicious\walnut\waffles\spam').mkdir(exist_ok=True)
+Path(r'/tmp/delicious/walnut/waffles/spam').mkdir(exist_ok=True)
 
 # Absolute and relative paths
 print(Path.cwd())
@@ -31,36 +33,107 @@ print(os.path.abspath('.\Scripts'))
 print(os.path.isabs('.'))
 print(os.path.isabs(os.path.abspath('.')))
 
-print(os.path.relpath(r'C:\Windows', r'C:\\' ))
-print(os.path.relpath(r'C:\Windows', r'C:\spam\eggs'))
+print(os.path.relpath(r'/tmp/delicious/walnut/waffles', r'/tmp'))
+print(os.path.relpath(r'/tmp/delicious/walnut/waffles', r'/var'))
 
 # Getting the parts of a file path
-p = Path('C:/Users/Al/spam.txt')
-print(p.anchor)                 # C:\
-print(p.parent)                 # C:\Users\Al
-print(p.name)                   # spam.txt
-print(p.stem)                   # spam
-print(p.suffix)                 # .txt
-print(p.drive)                  # C:
+p = Path('/tmp/spam.txt')
+print(p.anchor)
+print(p.parent)
+print(p.name)
+print(p.stem)
+print(p.suffix)
+print(p.drive)
 
 print(Path.cwd())
 print(Path.cwd().parents[0])
-print(Path.cwd().parents[1])
-print(Path.cwd().parents[2])
+# print(Path.cwd().parents[1])
+# print(Path.cwd().parents[2])
 
-calcFilePath = r'C:\Windows\System32\calc.exe'
+calcFilePath = r'/tmp/calc'
 print(os.path.basename(calcFilePath))
 print(os.path.dirname(calcFilePath))
 print(os.path.split(calcFilePath))
 print(calcFilePath.split(os.sep))
 
 # Finding file sizes and folder contents
-print(os.path.getsize(r'C:\Windows\System32\calc.exe'))
-print(os.listdir(r'C:\Windows\System32'))
+print(os.path.getsize(r'/bin/uname'))
+print(os.listdir(r'/bin'))
 
 totalSize = 0
-for filename in os.listdir(r'C:\Windows\System32'):
-    totalSize += os.path.getsize(os.path.join(r'C:\Windows\System32', filename))
+for filename in os.listdir(r'/tmp'):
+    totalSize += os.path.getsize(os.path.join(r'/tmp', filename))
 print(totalSize)
 
 # Modifying a list of files using glob patterns
+p = Path('/tmp')
+p = Path.home()
+print(p.glob('*'))              # generator
+print(list(p.glob('*')))        # list
+print(list(p.glob('*.txt')))    # list
+print(list(p.glob('*.log')))    # list
+
+for textFilePathObj in p.glob('*.txt'):
+    print(textFilePathObj)
+
+# Checking Path Validity
+optDir = Path('/opt')
+notExistsDir = Path('/folder/does/not/exist')
+unameFile = Path('/bin/uname')
+print(optDir.exists())
+print(optDir.is_dir())
+print(notExistsDir.exists())
+print(unameFile.is_file())
+print(unameFile.is_dir())
+
+# The File reading writing process
+from pathlib import Path
+p = Path('spam.txt')
+print(p.write_text('Hello, world!'))
+print(p.read_text())
+
+helloFile = open(Path.home() / 'hello.txt')
+helloContent = helloFile.read()
+print(helloContent)
+
+baconFile = open('bacon.txt', 'w')
+baconFile.write('Hello, world!\n')
+baconFile.close()
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable.')
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+print(content)
+
+# Saving variables with the shelve module
+import shelve
+shelfFile = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Simon']
+shelfFile['cats'] = cats
+shelfFile.close()
+
+shelfFile = shelve.open('mydata')
+print(type(shelfFile))
+print(shelfFile['cats'])
+shelfFile.close()
+print('-' * 40)
+shelfFile = shelve.open('mydata')
+print(list(shelfFile.keys()))
+print(list(shelfFile.values()))
+shelfFile.close()
+
+print('-' * 40)
+import pprint
+cats = [{'name': 'Zophie', 'desc': 'chubby'}, {'name': 'Pooka', 'desc': 'fluffy'}]
+pprint.pformat(cats)
+fileObj = open('myCats.py', 'w')
+fileObj.write('cats = ' + pprint.pformat(cats) + '\n')
+fileObj.close()
+
+print('-' * 40)
+import myCats
+print(myCats.cats)
+print(myCats.cats[0])
+print(myCats.cats[0]['name'])
